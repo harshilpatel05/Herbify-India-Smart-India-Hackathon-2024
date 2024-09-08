60b34ca32b39ad143a14c0ccf22f14e1c950310c
Harshil Patel <patelharshil2005@gmail.com>
2024-09-08 22:45:48 +0530

    Add files via upload

---
+++ virtual.js
@@ -0,0 +1,18 @@
+function redirectToAyush() {
+  window.open('https://ayush.gov.in');
+}
+function redirectToContact(){
+  window.location.href = 'contact.html'
+}
+function redirectToHome(){
+  window.location.href = "index.html"
+}
+function redirectToAbout(){
+  window.location.href = "aboutus.html"
+}
+function redirectToGallery(){
+  window.location.href = "gallery.html"
+}
+function redirectToVirtual(){
+  window.location.href = "virtual.html"
+}
